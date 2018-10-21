f7f8c2198ee22f60532d5add69d567f61ca7fa31
qtrsaoud <43649960+qtrsaoud@users.noreply.github.com>
2018-10-21 19:02:28 +0100

    Add files via upload

---
+++ week2(1).py
@@ -0,0 +1,5 @@
+number_of_pupils = int (input ('Enter the pupils: '))
+number_of_sweets = int (input ('Enter the sweets: '))
+
+print(number_of_sweets//number_of_pupils)
+print(number_of_sweets%number_of_pupils)
